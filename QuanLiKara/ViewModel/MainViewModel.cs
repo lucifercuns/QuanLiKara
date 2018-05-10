1f655f55fda6d4745a545cd2706bf6f0a2b872d5
TuanDoan_632 <1512632@student.hcmus.edu.vn>
2018-05-10 08:57:11 +0700

    Khởi tạo dự án
    Xây dựng project theo chuẩn MVVM

---
+++ QuanLiKara/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLiKara.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        //mọi thứ xử lí nằm trong này
+        public MainViewModel()
+        {
+            MessageBox.Show("Done");
+        }
+    }
+}
